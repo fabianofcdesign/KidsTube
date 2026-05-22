d4d132b44125fb580f929c0983b93de6a799a621
fabianosupercacheta <fabianosupercacheta@gmail.com>
2026-05-22 18:46:17 -0300

    feat: setup google client ID for real youtube syncing

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.tsx'
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'COLOQUE_SEU_CLIENT_ID_AQUI.apps.googleusercontent.com';
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '474933069332-erdedgoaiujpeu58nog50g5ffcg1h764.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
